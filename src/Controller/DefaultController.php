a4de68ba969f6a0c0664e7da5d6f4a28dcbd2061
Petru <smart__petea@mail.ru>
2018-10-08 21:29:17 +0300

    api info

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use GuzzleHttp\Client as HttpClient;
 
 class DefaultController extends AbstractController
 {
@@ -12,8 +13,9 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        $httpResponse = (new HttpClient())->get('http://api.ipstack.com/'.'94.139.138.228'.'?access_key=246cc7ab52f78603ca450b486417b339');
+        $geodata = json_decode($httpResponse->getBody()->getContents(), true);
+
+        return $this->render('default/index.html.twig', $geodata);
     }
 }
